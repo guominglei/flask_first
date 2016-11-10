8022a39bfd1ec92d8f8eafe01dee600357ca41bf
guominglei <rickguominglei@hotmail.com>
2016-11-10 16:57:42 +0800

    修改

--- test/basic_test.py
+++ test/basic_test.py
@@ -12,6 +12,11 @@ class TestBasic(object):
         res = client.get(url_for('login'))
         assert res.status_code == 200
 
+    def test_add(self, client):
+        response = client.post("/add/")
+        print response
+        assert response.status_code == 401
+
 
 def test_login(client):
     res = client.get("/login/")
